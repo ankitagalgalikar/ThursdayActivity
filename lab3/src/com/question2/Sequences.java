45b493e825077819e9fc24f1b8e708facc4a0c95
Ankita <Ankita@LAPTOP-69HHI633>
2022-12-02 19:03:14 +0530

    lab3

---
+++ lab3/src/com/question2/Sequences.java
@@ -0,0 +1,31 @@
+//Author:Ankita Galgalikar
+//To find sequences of lowercase letters joined with underscore.
+
+package com.question2;
+
+public class Sequences {
+
+	public static void main(String[] args) {
+		
+		String s="Java_labactivity";
+		boolean b=false;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='_')
+			{
+				char c=s.charAt(i+1);
+				if(Character.isLowerCase(c))
+				{
+					b=true;
+					break;
+				}
+			}
+		}
+		
+		if(b)
+		System.out.println("matched");
+		else
+			System.out.println("not matched");
+	}
+
+}
