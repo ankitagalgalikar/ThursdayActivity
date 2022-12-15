5121a8e259cadddf55ede6b8cef3b8e85f3c7f05
Ankita <Ankita@LAPTOP-69HHI633>
2022-12-15 18:39:52 +0530

    labactivity5

---
+++ Activity5/src/com/question2/LongestText.java
@@ -0,0 +1,32 @@
+//Author:Ankita Galgalikar
+//To find the longest word in a text file
+package com.question2;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class LongestText {
+	public String longestword() throws FileNotFoundException
+	{
+		String longword=" ";
+		String currentword;
+		Scanner sc=new Scanner(new File("C:\\javaprogram\\a.txt\\"));
+		while(sc.hasNext())
+		{
+			currentword=sc.next();		
+			if(currentword.length()>longword.length())		//comparing the current word and longest word
+			{
+				longword=currentword;
+			}
+		}
+		System.out.println("\n longets word in the file is "+longword);
+				return longword;
+		
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		new LongestText().longestword();
+		
+
+	}
+
+}
