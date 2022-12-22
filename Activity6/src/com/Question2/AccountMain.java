06cd9ba96cc534ddb172bceb8c7c9333b9a33a08
Ankita <Ankita@LAPTOP-69HHI633>
2022-12-22 18:19:01 +0530

    lab6

---
+++ Activity6/src/com/Question2/AccountMain.java
@@ -0,0 +1,22 @@
+//Author:Ankita Galgalikar
+//
+package com.Question2;
+public class AccountMain {
+	
+	public static void main(String[] args) {
+		Account a=new Account();		//creating a object
+		a.deposite();			//calling a method
+		try
+		{
+			//if condition becomes the false throws the exception
+			a.withdraw();		//calling a method
+			
+		}
+		catch(Exception e)		//the exception throws by the try block catch block catch that exception
+		{
+			System.out.println(e);
+		}
+		 
+	}
+
+}
